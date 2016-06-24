7cbe1ccfb8ffa279f205d69d57b249ac2e279db1
Nonnus <np@hellofresh.com>
2016-06-24 16:45:46 +0200

    removed NUIParser from cocoa pods, created framework

---
+++ Demo/NUI/NUI/Core/Renderers/NUIActivityIndicatorRenderer.h
@@ -0,0 +1,16 @@
+//
+//  NUIActivityIndicatorRenderer.h
+//  Pods
+//
+//  Created by Gomez, Antonio on 22/04/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NUIActivityIndicatorRenderer : NSObject
+
++ (void)render:(UIActivityIndicatorView*)activityIndicator withClass:(NSString*)className;
+
+@end
---
+++ Demo/NUI/NUI/Core/Renderers/NUITextInputTraitsRenderer.h
@@ -0,0 +1,16 @@
+//
+//  NUITextInputTraitsRenderer.h
+//  NUIDemo
+//
+//  Created by Alexey Trenikhin on 3/13/16.
+//  Copyright © 2016 Tom Benner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NUITextInputTraitsRenderer : NSObject
+
++ (void)renderKeyboard:(id<UITextInputTraits>) traits withClass:(NSString*)className;
+
+@end
