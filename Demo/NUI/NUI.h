792ae4ab5d2fb3294abcf0a3a807d219e2d0fe74
Peter Prokop <pitrowech@gmail.com>
2016-06-27 17:57:22 +0200

    Add import NUISettings in NUI.h

--- Demo/NUI/NUI.h
+++ Demo/NUI/NUI.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char NUIVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <NUI/PublicHeader.h>
 
 
+#import <NUI/NUISettings.h>
